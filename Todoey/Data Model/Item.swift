b21b9b569945bdb7ef09936768afe6ec21f9a21c
Miguel Franco <miguelfranco@miguels-mbp.mynet>
2018-11-28 00:33:59 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Miguel Franco on 28/11/2018.
+//  Copyright © 2018 Miguel Franco. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
